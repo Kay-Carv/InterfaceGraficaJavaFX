d6969b28d142b7459d611f6ea4eb0c5dba366ab3
Kayque <kayquecarvalho020@gmail.com>
2025-10-13 11:40:49 -0300

    Feat: Adicionando mais conteúdo no menu lateral

--- src/main/java/br/com/fiap2espa/demo/ApplicationGUI.java
+++ src/main/java/br/com/fiap2espa/demo/ApplicationGUI.java
@@ -1,11 +1,16 @@
 package br.com.fiap2espa.demo;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.robot.Robot;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class ApplicationGUI extends Application {
@@ -20,13 +25,33 @@ public class ApplicationGUI extends Application {
 
         //
         VBox leftMenu = new VBox(20); //spacing 20 -> margin base
+        leftMenu.setPadding(new Insets(10));
 
         Label menuTitle = new Label("Menu");
-        Button btn = new Button("Clique aqui");
+        Button btnCreate = new Button("Cadastrar");
+        Button btnRead = new Button("Listar amigos");
+        Button btnUpdate = new Button("Atualizar dados");
+        Button btnDelete = new Button("Excluir amigo");
+        Button btnExit = new Button("Sair do programa");
 
         //Instanciando dentro do menu lateral
         leftMenu.getChildren().add(menuTitle);
-        leftMenu.getChildren().add(btn);
+        leftMenu.getChildren().addAll( // .addAll para adicionar todos
+                btnCreate,
+                btnRead,
+                btnUpdate,
+                btnDelete,
+                btnExit);
+
+        //Modificando tamanho dos botões
+        menuTitle.setPrefSize(40, 20);
+        menuTitle.setAlignment(Pos.CENTER);
+        menuTitle.setFont(Font.font("Verdana", FontWeight.BOLD, 13));
+        btnCreate.setPrefSize(120, 33);
+        btnRead.setPrefSize(120, 33);
+        btnUpdate.setPrefSize(120, 33);
+        btnDelete.setPrefSize(120, 33);
+        btnExit.setPrefSize(120, 33);
 
         root.setLeft(leftMenu);
 
